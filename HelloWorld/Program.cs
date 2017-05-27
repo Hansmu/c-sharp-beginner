6821d85b1f2986247399e0f079529669e8c3eb80
hansmu <hans.miikael@gmail.com>
2017-05-27 14:06:26 +0300

    First hello world example.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// Have access to all the classes that are within one namespace. In order to use one from another namespace, then you need to use "using" to import.
+
+namespace HelloWorld
+{
+	class Program
+	{
+		// Main is the entry point of the application. Case sensitive.
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world");
+		}
+	}
+}
